e93ab88eb375c10cf670539e2fd55784e0a75214
Gustavo Henrique <guazambujinha@gmail.com>
2025-04-05 17:12:15 -0300

    fist commit

---
+++ API_Restaurante/src/main/java/API_Restaurante/ApiRestauranteApplication.java
@@ -0,0 +1,13 @@
+package API_Restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestauranteApplication.class, args);
+	}
+
+}
